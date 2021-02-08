ce3d9d7ab0a5800e1af017840a8926759b2ba04f
Добавил файлы <realkraizer@gmail.com>
2021-02-09 00:13:40 +0300

    deleted some files

--- SITE/WP_FILES/wp-content/plugins/shortcodes-ultimate/admin/partials/help/addons.php
+++
@@ -1,5 +0,0 @@
-<?php defined( 'ABSPATH' ) or exit; ?>
-
-<ul>
-	<li><a href="https://getshortcodes.com/docs-category/addons/" target="_blank"><?php _e( 'Full add-ons documentation', 'shortcodes-ultimate' ); ?></a></li>
-</ul>
